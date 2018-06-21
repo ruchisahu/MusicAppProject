5d0edc31708753d66fd41037b0d196a5b55a9a5e
ruchisahu <sahruchi@gmail.com>
2018-06-21 00:37:05 -0700

    login

---
+++ Authenticate/Authenticate/Config/AuthHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authenticate.Config
+{
+   public  class AuthHelper
+    {
+        private static Dictionary<string, string> dict = new Dictionary<string, string>();
+        public void AddUser(string username,string password)
+        {
+           // dict.Add("Ruchi", "101");
+           dict.Add(username, password);
+
+        }
+        public static bool Check(string username, string password)
+        {
+            // if(dict.ContainsKey("username")
+            for (int i = 0; i < dict.Count; i++)
+            {
+
+                if (dict.Keys.ElementAt(i) == username)
+                {
+                    if (dict[username] == password)
+                     //   Console.WriteLine("match");
+                    return true;
+                }
+            }
+          //  Console.WriteLine("try again");
+            return false;
+        }
+
+          
+          
+    }
+    
+}
---
+++ Authenticate/Authenticate/Model/Authentication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Authenticate.Model
+{
+    class Authentication
+    {
+        public static bool Authenticate1(string UserName, string Password)
+        {
+            return true; 
+        }
+    }
+}
+    
+
